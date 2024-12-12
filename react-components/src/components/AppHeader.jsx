547027233808fb97d560d78667ed870ceee2002a
sebastianosalzillo <salzillosebastiano@gmail.com>
2024-12-12 16:19:07 +0100

    Feat : time modifiche

--- react-components/src/components/AppHeader.jsx
+++ react-components/src/components/AppHeader.jsx
@@ -2,7 +2,7 @@ import style from './AppHeader.module.css'
 function AppHeader() {
     return (
     <>
-    <h1> Il mio Blog</h1>
+   <h1 className={style.title}>Il mio Blog</h1>
     </>
     )
 }
